9c7829e164b3178ca584bdbdce9eb28dbc4dd18d
notintoab <87016317+notintoab@users.noreply.github.com>
2025-11-28 13:23:45 +0100

    chore: migrate terraform state to s3 backend

---
+++ backend_resources.tf
@@ -0,0 +1,56 @@
+#  Create an S3 bucket
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "sentinel-terraform-state-bucket-456" # Use a unique name
+  
+  # Prevent accidental deletion
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "Terraform Remote State"
+  }
+}
+
+# Enable versioning for the bucket
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable encryption for the bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block public access to the bucket 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Create a DynamoDB table for locks
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "sentinel-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"          # Required key name for Terraform
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
--- versions.tf
+++ versions.tf
@@ -6,4 +6,14 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
+
+  # Configure the backend to use S3 and DynamoDB for state management
+  backend "s3" {
+    bucket         = "sentinel-terraform-state-bucket-456"
+    key            = "sentinel/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "sentinel-terraform-locks"
+    encrypt        = true
+  }
+
 }
